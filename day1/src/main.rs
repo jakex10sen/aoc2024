bdecb8dafc3945a25647e89c80fadd3e9d806a5a
Jacob Christensen <jakex10sen@gmail.com>
2024-12-03 01:06:38 -0500

    Updated input path

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     // 1. Read from input
-    let file_path = "day1/input";
+    let file_path = "input";
     println!("In file {file_path}");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
