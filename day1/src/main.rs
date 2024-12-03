9fb0ec821e383bd76e5f4453235a327c534ac789
Jacob Christensen <jakex10sen@gmail.com>
2024-12-02 23:43:14 -0500

    Initial commit

---
+++ day1/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // 1. Read from input
+    // 2. Parse out pairs into lists
+    // 3. Sort each lists
+    // 4. Pair the lists and compute the distance
+    // 5. Add distance to the sum
+    // 6. Output the sum
+    //
+    println!("Hello, world!");
+}
